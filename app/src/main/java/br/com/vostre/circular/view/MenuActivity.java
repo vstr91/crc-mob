84d91cf618471f569658def798cfcfed98c8661f
Almir <almir.amjunior@gmail.com>
2018-04-24 17:46:50 -0300

    Commit Inicial

---
+++ app/src/main/java/br/com/vostre/circular/view/MenuActivity.java
@@ -0,0 +1,15 @@
+package br.com.vostre.circular.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.com.vostre.circular.R;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
---
+++ circleview/src/main/java/br/com/vostre/circleview/CircleView.java
@@ -0,0 +1,236 @@
+package br.com.vostre.circleview;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Almir on 28/02/2018.
+ */
+
+public class CircleView extends View {
+
+    private float mWidth;
+    private float mHeight;
+    private Paint paint;
+
+    int corArcoMaior;
+    int corArcoMenor;
+    int espessuraArcoMaior;
+    int espessuraArcoMenor;
+
+    Drawable imagem;
+    Bitmap b;
+
+    Rect rectImagem;
+    RectF rectF;
+
+    public int getCorArcoMaior() {
+        return corArcoMaior;
+    }
+
+    public void setCorArcoMaior(int corArcoMaior) {
+        this.corArcoMaior = corArcoMaior;
+        invalidate();
+    }
+
+    public int getCorArcoMenor() {
+        return corArcoMenor;
+    }
+
+    public void setCorArcoMenor(int corArcoMenor) {
+        this.corArcoMenor = corArcoMenor;
+        invalidate();
+    }
+
+    public int getEspessuraArcoMaior() {
+        return espessuraArcoMaior;
+    }
+
+    public void setEspessuraArcoMaior(int espessuraArcoMaior) {
+        this.espessuraArcoMaior = espessuraArcoMaior;
+        invalidate();
+    }
+
+    public int getEspessuraArcoMenor() {
+        return espessuraArcoMenor;
+    }
+
+    public void setEspessuraArcoMenor(int espessuraArcoMenor) {
+        this.espessuraArcoMenor = espessuraArcoMenor;
+        invalidate();
+    }
+
+    public Drawable getImagem() {
+        return imagem;
+    }
+
+    public void setImagem(Drawable imagem) {
+        this.imagem = imagem;
+        invalidate();
+    }
+
+    public CircleView(Context context) {
+        super(context);
+        init(context, null);
+    }
+
+    public CircleView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init(context, attrs);
+    }
+
+    public CircleView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context, attrs);
+    }
+
+    private void init(Context context, AttributeSet attrs) {
+
+        corArcoMaior = Color.GRAY;
+        corArcoMenor = Color.GRAY;
+        espessuraArcoMaior = 50;
+        espessuraArcoMenor = 5;
+
+        imagem = null;
+
+        // checa atributos xml
+        if (attrs != null) {
+            TypedArray typedArray = getContext().obtainStyledAttributes(attrs,
+                    R.styleable.CircleView,
+                    0, 0);
+
+            // valores atributos xml
+            corArcoMaior = typedArray.getColor(R.styleable.CircleView_corArcoMaior,
+                    corArcoMaior);
+            corArcoMenor = typedArray.getColor(R.styleable.CircleView_corArcoMenor,
+                    corArcoMenor);
+            espessuraArcoMaior = typedArray.getInteger(R.styleable.CircleView_espessuraArcoMaior,
+                    espessuraArcoMaior);
+            espessuraArcoMenor = typedArray.getInteger(R.styleable.CircleView_espessuraArcoMenor,
+                    espessuraArcoMenor);
+            imagem = typedArray.getDrawable(R.styleable.CircleView_imagem);
+            typedArray.recycle();
+
+        }
+
+        paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        paint.setColor(corArcoMenor);
+
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        // Calculate the radius from the width and height.
+        mWidth = w;
+        mHeight = h;
+
+        if(imagem != null){
+            b = Bitmap.createScaledBitmap(drawableToBitmap(imagem), (int) mWidth, (int) mHeight, true);
+            b = getCircularBitmap(b);
+
+            int left = 0;
+            int top = 0;
+            int right = (int) mWidth;
+            int bottom = (int) mHeight;
+
+            rectImagem = new Rect(left, top, right, bottom);
+        }
+
+        rectF = new RectF(espessuraArcoMaior, espessuraArcoMaior, mWidth-espessuraArcoMaior, mHeight-espessuraArcoMaior);
+
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        if(imagem != null){
+            canvas.drawBitmap(b, rectImagem, rectF, paint);
+        }
+
+        // desenhando arco menor
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(espessuraArcoMenor);
+        paint.setStrokeCap(Paint.Cap.ROUND);
+        paint.setColor(corArcoMenor);
+
+        canvas.drawArc(rectF, -90, 180, false, paint);
+
+        // desenhando arco maior
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(espessuraArcoMaior);
+        paint.setStrokeCap(Paint.Cap.ROUND);
+        paint.setColor(corArcoMaior);
+
+        canvas.drawArc(rectF, 90, 180, false, paint);
+
+    }
+
+    public static Bitmap drawableToBitmap (Drawable drawable) {
+        Bitmap bitmap = null;
+
+        if (drawable instanceof BitmapDrawable) {
+            BitmapDrawable bitmapDrawable = (BitmapDrawable) drawable;
+            if(bitmapDrawable.getBitmap() != null) {
+                return bitmapDrawable.getBitmap();
+            }
+        }
+
+        if(drawable.getIntrinsicWidth() <= 0 || drawable.getIntrinsicHeight() <= 0) {
+            bitmap = Bitmap.createBitmap(1, 1, Bitmap.Config.ARGB_8888); // Single color bitmap will be created of 1x1 pixel
+        } else {
+            bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+        }
+
+        Canvas canvas = new Canvas(bitmap);
+        drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+        drawable.draw(canvas);
+        return bitmap;
+    }
+
+    public static Bitmap getCircularBitmap(Bitmap bitmap) {
+        Bitmap output;
+
+        if (bitmap.getWidth() > bitmap.getHeight()) {
+            output = Bitmap.createBitmap(bitmap.getHeight(), bitmap.getHeight(), Bitmap.Config.ARGB_8888);
+        } else {
+            output = Bitmap.createBitmap(bitmap.getWidth(), bitmap.getWidth(), Bitmap.Config.ARGB_8888);
+        }
+
+        Canvas canvas = new Canvas(output);
+
+        final int color = 0xff424242;
+        final Paint paint = new Paint();
+        final Rect rect = new Rect(0, 0, bitmap.getWidth(), bitmap.getHeight());
+
+        float r = 0;
+
+        if (bitmap.getWidth() > bitmap.getHeight()) {
+            r = bitmap.getHeight() / 2;
+        } else {
+            r = bitmap.getWidth() / 2;
+        }
+
+        paint.setAntiAlias(true);
+        canvas.drawARGB(0, 0, 0, 0);
+        paint.setColor(color);
+        canvas.drawCircle(r, r, r, paint);
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        canvas.drawBitmap(bitmap, rect, rect, paint);
+        return output;
+    }
+
+}

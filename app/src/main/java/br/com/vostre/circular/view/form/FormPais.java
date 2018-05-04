10e80a8c28644f1f5110f1ae10f7cbed978fe753
Almir <almir.amjunior@gmail.com>
2018-05-04 17:58:11 -0300

    Desenvolvendo form paises

--- app/src/main/java/br/com/vostre/circular/view/form/FormPais.java
+++ app/src/main/java/br/com/vostre/circular/view/form/FormPais.java
@@ -1,5 +1,6 @@
 package br.com.vostre.circular.view.form;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
@@ -9,18 +10,27 @@ import android.view.ViewGroup;
 import android.view.Window;
 
 import br.com.vostre.circular.R;
+import br.com.vostre.circular.databinding.FormPaisBinding;
 
 public class FormPais extends DialogFragment {
 
+    FormPaisBinding binding;
+
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.form_pais, container, false);
+//        View view = inflater.inflate(R.layout.form_pais, container, false);
+//
+//        if(this.getDialog() != null){
+//            this.getDialog().requestWindowFeature(Window.FEATURE_NO_TITLE);
+//        }
+//
+//        return view;
 
-        if(this.getDialog() != null){
-            this.getDialog().requestWindowFeature(Window.FEATURE_NO_TITLE);
-        }
-
-        return view;
+        binding = DataBindingUtil.inflate(
+                inflater, R.layout.form_pais, container, false);
+        super.onCreate(savedInstanceState);
+        binding.setView(this);
+        return binding.getRoot();
 
     }
 
